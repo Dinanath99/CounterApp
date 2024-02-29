6c1c83efa051fc68c9103ffe3acc194af2f8f926
Dinanath <dkmdkm26@gmail.com>
2024-02-29 16:07:19 +0545

    Counter app using react

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(0);
+  // counting the value
+  const increment = () => {
+    if (count >= 20) {
+      alert("you can not increase more than 20");
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const decrement = () => {
+    if (count <= 0) {
+      alert("you can not decrease more than 0");
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>Chai or React</h1>
+      <h2>Counter value : {count}</h2>
+
+      <button id="increase" onClick={increment}>
+        increase Count
+      </button>
+      <br />
+      <br />
+      <button id="decrease" onClick={decrement}>
+        Decrease Count
+      </button>
+      <p>Couning the value : {count}</p>
+    </>
+  );
+}
+
+export default App;
